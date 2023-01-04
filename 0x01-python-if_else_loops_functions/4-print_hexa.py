2730ea399d8bc1fe86ffd22b8d7d69401ecefb05
Khali3sa <yvonnekhaliesa@gmail.com>
2023-01-04 08:58:05 -0800

    loops commit

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# Author - Yvonne Kaliyesa
+
+"""print numbers 0 t0 98 in decimal and hexidecimal."""
+for numbe in range(0, 99):
+    print("{} = {}".format(number, hex(number)))
---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Author - Yvonne Kaliyesa
+
+def print_last_digit(number):
+    print(abs(number) % 10, end="")
